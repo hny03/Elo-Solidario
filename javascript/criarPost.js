23a2dacb73a17af9542104342e189f2203ba1ce0
mmesqigor <mmesquita.igor@gmail.com>
2025-08-20 00:54:10 -0300

    Página de perfil v.1.1

---
+++ javascript/criarPost.js
@@ -0,0 +1,51 @@
+function criarPost(dadosPost) {
+    const postID = `post-${Date.now()}`;
+
+    let classeTipo = dadosPost.tipo;
+    let textoTipo = (dadosPost.tipo === "trabalhoVoluntario") ? "Trabalho Voluntário" : "Doação";
+
+    let classeModalidade = dadosPost.modalidade;
+    let textoModalidade = (dadosPost.modalidade === "presencial") ? "Presencial" : "Remoto";
+
+    const postHTML = `
+        <section id="${postID}" class="containerPost mb-3">
+            <div class="row p-3 pb-0">
+                <div class="col-lg-6">
+                <h2>${dadosPost.titulo}</h2>
+                </div>
+                <div class="col-lg-3">
+                    <div class="row">
+                        <div class="col-lg-2">
+                            <img src="../imagens/local.png" width="20" height="20">
+                        </div>
+                        <div class="col-lg-10">
+                            <p>${dadosPost.local}</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-2">
+                            <img src="../imagens/relogio.png" width="20" height="20">
+                        </div>
+                        <div class="col-lg-10">
+                            <p>${dadosPost.data} às ${dadosPost.horario}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <p class="${classeTipo}">${textoTipo}</p>
+                    <p class="${classeModalidade}">${textoModalidade}</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-1 offset-lg-11">
+                    <button type="button" class="btn btn-secondary transparentButton p-0 mb-2 ms-2" 
+                        data-bs-toggle="modal" data-bs-target="#modalEditarPost" data-post-id="${postID}">
+                        <img src="../imagens/ampliar.png" width="25" height="25">
+                    </button>
+                </div>
+            </div>
+        </section>
+        `
+
+    return postHTML;
+}
--- javascript/validarForms.js
+++ javascript/validarForms.js
@@ -1,16 +1,31 @@
 $(function(){
+    let idPostExcluir = null;
+
     /* Valida criação de post */
     $("#modalCriarPost .btn-success").click(function(){
         const titulo = $("#tituloCriarPost").val();
+        const local = $("#localCriarPost").val();
         const data = $("#dataCriarPost").val();
         const horario = $("#horarioCriarPost").val();
         const tipo = $("#tipoCriarPost").val();
         const modalidade = $("#modalidadeCriarPost").val();
 
+        const dadosPost = {
+            titulo,
+            local,
+            data,
+            horario,
+            tipo,
+            modalidade
+        };
+
         if(titulo.trim() === "" || data.trim() === "" || horario.trim() === "" || 
             tipo.trim() === "-" || modalidade.trim() === "-") {
             alert("Campos marcados com (*) devem ser preenchidos!");
         } else {
+            const novoPostHTML = criarPost(dadosPost);
+
+            $("#feedPost").prepend(novoPostHTML);
             $("#modalCriarPost .btn-secondary").click();
         }
     });
@@ -28,6 +43,18 @@ $(function(){
             alert("Campos marcados com (*) devem ser preenchidos!");
         } else {
             $("#modalEditarPost .btn-secondary").click();
+        }
+    });
+
+    /* Valida exclusão de post */
+    $("#feedPost").on("click", "[data-bs-target='#modalEditarPost']", function(){
+        idPostExcluir = $(this).data("post-id");
+    });
+
+    $("#modalExcluirPost .btn-danger").click(function(){
+        if(idPostExcluir) {
+            $(`#${idPostExcluir}`).remove();
+            idPostExcluir = null;
         }
     });
 
