b3cef17048091d140d86fa793a5a993611bd2ab6
mmesqigor <mmesquita.igor@gmail.com>
2025-08-20 16:24:52 -0300

    Implementada edição de post

--- javascript/criarPost.js
+++ javascript/criarPost.js
@@ -7,11 +7,21 @@ function criarPost(dadosPost) {
     let classeModalidade = dadosPost.modalidade;
     let textoModalidade = (dadosPost.modalidade === "presencial") ? "Presencial" : "Remoto";
 
+    const dataTratado = dadosPost.data.split("/");
+    const dataInput = `${dataTratado[2]}-${dataTratado[1]}-${dataTratado[0]}`;
+
     const postHTML = `
-        <section id="${postID}" class="containerPost mb-3">
+        <section id="${postID}" class="containerPost mb-3" 
+            id="${postID}" 
+            data-titulo="${dadosPost.titulo}" 
+            data-local="${dadosPost.local}" 
+            data-data="${dadosPost.data}" 
+            data-horario="${dadosPost.horario}" 
+            data-tipo="${dadosPost.tipo}" 
+            data-modalidade="${dadosPost.modalidade}">
             <div class="row p-3 pb-0">
                 <div class="col-lg-6">
-                <h2>${dadosPost.titulo}</h2>
+                <h2 id="tituloPost">${dadosPost.titulo}</h2>
                 </div>
                 <div class="col-lg-3">
                     <div class="row">
@@ -19,7 +29,7 @@ function criarPost(dadosPost) {
                             <img src="../imagens/local.png" width="20" height="20">
                         </div>
                         <div class="col-lg-10">
-                            <p>${dadosPost.local}</p>
+                            <p id="localPost">${dadosPost.local}</p>
                         </div>
                     </div>
                     <div class="row">
@@ -27,13 +37,13 @@ function criarPost(dadosPost) {
                             <img src="../imagens/relogio.png" width="20" height="20">
                         </div>
                         <div class="col-lg-10">
-                            <p>${dadosPost.data} às ${dadosPost.horario}</p>
+                            <p id="dataHorarioPost">${dadosPost.data} às ${dadosPost.horario}</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3">
-                    <p class="${classeTipo}">${textoTipo}</p>
-                    <p class="${classeModalidade}">${textoModalidade}</p>
+                    <p id="tipoPost" class="${classeTipo}">${textoTipo}</p>
+                    <p id="modalidadePost" class="${classeModalidade}">${textoModalidade}</p>
                 </div>
             </div>
             <div class="row">
---
+++ javascript/editarPost.js
@@ -0,0 +1,62 @@
+$(function(){
+    idPostEditar = null;
+
+    /* Preenche os inputs com os valores já existentes do Post quando abrir a edição */
+    $("#feedPost").on("click", "[data-bs-target='#modalEditarPost']", function(){
+        const post = $(this).closest(".containerPost");
+
+        idPostEditar = post.attr("id");
+
+        const dados = {
+            titulo: post.data("titulo"),
+            local: post.data("local"),
+            data: post.data("data"),
+            horario: post.data("horario"),
+            tipo: post.data("tipo"),
+            modalidade: post.data("modalidade"),
+        }
+
+        $("#tituloEditarPost").val(dados.titulo);
+        $("#localEditarPost").val(dados.local);
+        $("#dataEditarPost").val(dados.data);
+        $("#horarioEditarPost").val(dados.horario);
+        $("#tipoEditarPost").val(dados.tipo);
+        $("#modalidadeEditarPost").val(dados.modalidade);
+    });
+
+    function SalvarEdicao() {
+        if(!idPostEditar) {
+            alert("Erro ao selecionar post para edição.");
+            return;
+        }
+
+        const post = $(`#${idPostEditar}`);
+
+        const novosDados = {
+            titulo: $("#tituloEditarPost").val(),
+            local: $("#localEditarPost").val(),
+            data: $("#dataEditarPost").val(),
+            horario: $("#horarioEditarPost").val(),
+            tipo: $("#tipoEditarPost").val(),
+            modalidade: $("#modalidadeEditarPost").val(),
+        }
+
+        const textoTipo = (novosDados.tipo === "trabalhoVoluntario") ? "Trabalho Voluntário" : "Doação";
+        const textoModalidade = (novosDados.modalidade === "presencial") ? "Presencial" : "Remoto";
+
+        post.find("#tituloPost").text(novosDados.titulo);
+        post.find("#localPost").text(novosDados.local);
+        post.find("#dataHorarioPost").text(`${novosDados.data} às ${novosDados.horario}`);
+        post.find("#tipoPost").text(textoTipo).attr("class", novosDados.tipo);
+        post.find("#modalidadePost").text(textoModalidade).attr("class", novosDados.modalidade);
+
+        post.data("titulo", novosDados.titulo);
+        post.data("local", novosDados.local);
+        post.data("data", novosDados.data);
+        post.data("horario", novosDados.horario);
+        post.data("tipo", novosDados.tipo);
+        post.data("modalidade", novosDados.modalidade);
+    }
+
+    $("#modalEditarPost .btn-success").click(SalvarEdicao);
+});
